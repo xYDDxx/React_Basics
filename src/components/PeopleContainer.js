99e560840395006ad246b75c94dbfb5fb522d5f7
Ian <ianlee.felder@gmail.com>
2024-02-21 12:01:36 +0100

    Filter CardsData Fertig

--- src/components/PeopleContainer.js
+++ src/components/PeopleContainer.js
@@ -3,20 +3,39 @@ import Card from './Card'
 
 export default function PeopleContainer () {
   const [people, setPeople] = useState([])
+  const [filteredPeople, setFilteredPeople] = useState([])
 
   useEffect(() => {
     fetch('https://65cf56fabdb50d5e5f5b10c2.mockapi.io/person').then(res =>
       res.json().then(data => {
         setPeople(data)
+        setFilteredPeople(data)
       })
     )
   }, [])
 
+  const filterPeople = filter => {
+    let filtered = people.filter(person =>
+      person.name.toLowerCase().includes(filter.toLowerCase())
+    )
+    setFilteredPeople(filtered)
+  }
+
   return (
-    <div>
-      <h1>People</h1>
-      <div className='grid grid-cols-2 lg:grid-cols-4 gap-4'>
-        {people.map(person => {
+    <div className='p-8'>
+      <div className='fixed mb-4 bg-white w-full h-22 p-8'>
+        <input
+          className='border-2 border-black p-4 h-2'
+          type='text'
+          placeholder='search'
+          onChange={el => {
+            console.log(el.target.value)
+            filterPeople(el.target.value)
+          }}
+        ></input>
+      </div>
+      <div className='grid grid-cols-2 lg:grid-cols-4 gap-4 p-8 pt-32'>
+        {filteredPeople.map(person => {
           return (
             <Card
               name={person.name}
